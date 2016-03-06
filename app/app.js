f18738c3faec2b6623978eb8434dc45b5c3b270f
TommyCakes <tomhwarhurst@aol.com>
2016-03-06 14:58:59 +0000

    Resized picture, fixed nav item positions and added animation to nav

--- app/app.js
+++ app/app.js
@@ -25,25 +25,32 @@ angular.module('App', ['ui.router'])
     })
     .state('contact', {
       url: "/contact",
-      templateUrl: 'app/states/contact.html'
+      templateUrl: 'app/states/contact.html',
+      controller: 'PhotoCtrl as photo'
     })
   })
 
   .controller('PhotoCtrl', function($http) {
+
+   var scroll_start = 0;
+   var startchange = $('#top');
+   var offset = startchange.offset();
+  if (startchange.length){
+   $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".nav").css('background-color', '#2A2627');
+       }
+   });
+  }
+
     var self = this;
     var self = this;
-
-    // this.getRecentPictures = function() {
-
-    // var user = 'tommycake';
-    // var accessToken = '341290126.f5660bb.ba6292f44e2e4bc98e4a582998a639f3';
-    // var url = "https://api.instagram.com/v1/users/" + user + "/media/recent/?";
     var url = "https://api.instagram.com/v1/users/self/media/recent?access_token=2287225590.f96b4c5.30bb0ee0864046c396c120e20ca1cb29&count=30"
     var request = {
       callback: 'JSON_CALLBACK',
       // client_id: 'f5660bbde18946eda38e1b2b2da56266',
       client_id: '	f96b4c51676c4425b5de12ab8936e6a2',
-
     };
 
     $http({
